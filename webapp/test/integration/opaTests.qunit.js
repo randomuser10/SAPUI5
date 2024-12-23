2a1281a75f26be828a4d7117cef3bc43a7ffa79e
Mayur Satardekar <mayursatardekar10@gmail.com>
2024-12-23 13:22:03 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["projectapp/projectmds/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"projectapp/projectmds/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
