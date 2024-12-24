2e478a1f61cc9c09837905481858f9178e81e410
Mayur Satardekar <mayursatardekar10@gmail.com>
2024-12-24 14:10:30 +0000

    DynamicUIChange

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -13,6 +13,10 @@ sap.ui.define([
             var name = this.getView().byId("idInp").getValue();
             var msg = "Welcome to " + name;
             this.getView().byId("idTxt").setText(msg);
+            this.getView().byId("idbtn1").setType("Accept");
+            this.getView().byId("idTxt").setTextAlign("Left");
+            this.getView().byId("lbl1").setRequired(false);
+            this.getView().byId("idInp").setEnabled(false);
         }
         
         
